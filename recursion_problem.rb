8061de2e8815aa1bae8e686c306863eeca073cc8
MShajieShah <shajieshah@gmail.com>
2021-11-18 23:51:25 +0500

    Recursive examples

--- recursion_problem.rb
+++ recursion_problem.rb
@@ -37,3 +37,45 @@ end
 p factorial(0)
 
 #average
+def findavg(a)
+  n = a.size
+  if (n == 1)
+    return sum = a[0]
+  else
+    sum = a.pop
+    sum = sum + findavg(a)
+  end
+  sum
+end
+
+p findavg([1, 2, 3, 4, 5])
+
+#count down
+def countdown(n)
+  return if n == 0
+  puts n
+  countdown(n - 1)
+end
+
+p countdown(3)
+
+# factorial
+
+def fac(n)
+  if (n == 0 || n == 1)
+    return 1
+  else
+    return n * fac(n - 1)
+  end
+end
+
+p fac(5)
+
+def fun1(n)
+  if (n == 1)
+    return 0
+  else
+   p 1 + fun1(n / 2)
+  end
+end
+p fun1(7)
